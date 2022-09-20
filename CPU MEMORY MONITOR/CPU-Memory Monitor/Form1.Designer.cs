017a2354781cc3e9153d280032a55d567c484d72
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-09-20 17:50:09 +0200

    CPU Monitor

---
+++ CPU MEMORY MONITOR/CPU-Memory Monitor/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CPU_Memory_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBarCPU = new System.Windows.Forms.ProgressBar();
+            this.progressBarMEM = new System.Windows.Forms.ProgressBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.lblMem = new System.Windows.Forms.Label();
+            this.lblCpu = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Commodore 64 Angled", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU Usage:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Commodore 64 Angled", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Memory Usage:";
+            // 
+            // progressBarCPU
+            // 
+            this.progressBarCPU.Location = new System.Drawing.Point(87, 49);
+            this.progressBarCPU.Name = "progressBarCPU";
+            this.progressBarCPU.Size = new System.Drawing.Size(301, 23);
+            this.progressBarCPU.Step = 1;
+            this.progressBarCPU.TabIndex = 4;
+            // 
+            // progressBarMEM
+            // 
+            this.progressBarMEM.Location = new System.Drawing.Point(87, 136);
+            this.progressBarMEM.Name = "progressBarMEM";
+            this.progressBarMEM.Size = new System.Drawing.Size(301, 23);
+            this.progressBarMEM.Step = 1;
+            this.progressBarMEM.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CPU_Memory_Monitor.Properties.Resources.icons8_database_64;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 125);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 47);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CPU_Memory_Monitor.Properties.Resources.icons8_cpu_64;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(58, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.Lime;
+            this.btn_Start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Start.Font = new System.Drawing.Font("Commodore 64 Angled", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.Location = new System.Drawing.Point(12, 190);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(107, 49);
+            this.btn_Start.TabIndex = 6;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // lblMem
+            // 
+            this.lblMem.Font = new System.Drawing.Font("Commodore 64 Angled", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMem.Location = new System.Drawing.Point(343, 100);
+            this.lblMem.Name = "lblMem";
+            this.lblMem.Size = new System.Drawing.Size(45, 33);
+            this.lblMem.TabIndex = 7;
+            this.lblMem.Text = "0%";
+            this.lblMem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCpu
+            // 
+            this.lblCpu.Font = new System.Drawing.Font("Commodore 64 Angled", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpu.Location = new System.Drawing.Point(343, 12);
+            this.lblCpu.Name = "lblCpu";
+            this.lblCpu.Size = new System.Drawing.Size(45, 33);
+            this.lblCpu.TabIndex = 8;
+            this.lblCpu.Text = "0%";
+            this.lblCpu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(405, 258);
+            this.Controls.Add(this.lblCpu);
+            this.Controls.Add(this.lblMem);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.progressBarMEM);
+            this.Controls.Add(this.progressBarCPU);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(421, 297);
+            this.MinimumSize = new System.Drawing.Size(421, 297);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CPU Memory Monitor v.0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBarCPU;
+        private System.Windows.Forms.ProgressBar progressBarMEM;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label lblMem;
+        private System.Windows.Forms.Label lblCpu;
+    }
+}
+
---
+++ CPU MEMORY MONITOR/CPU-Memory Monitor/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CPU_Memory_Monitor
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer _timer;
+        bool startflag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 3000;
+            _timer.Elapsed += OntimedEvent;
+            _timer.AutoReset = true;
+
+        }
+        private void OntimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            // We will monitor and get the values of the cpu and memory
+            int cpuValue = GetCpuValue();
+            int memValue = GetMemValue();
+            // -----------------------------------------------------------------------------
+            if (progressBarCPU.InvokeRequired)
+            {
+                progressBarCPU.Invoke(new Action(() => progressBarCPU.Value = cpuValue));
+            }
+            else
+            {
+                progressBarCPU.Value = cpuValue;
+            }
+            // -----------------------------------------------------------------------------
+            if (lblCpu.InvokeRequired)
+            {
+                lblCpu.Invoke(new Action(() => lblCpu.Text = cpuValue.ToString() + " % "));
+            }
+            else
+            {
+                lblCpu.Text = cpuValue.ToString() + " % ";
+            }
+            // -----------------------------------------------------------------------------
+            if (progressBarMEM.InvokeRequired)
+            {
+                progressBarMEM.Invoke(new Action(() => progressBarMEM.Value = memValue));
+            }
+            else
+            {
+                progressBarMEM.Value = memValue;
+            }
+            // -----------------------------------------------------------------------------
+            if (lblMem.InvokeRequired)
+            {
+                lblMem.Invoke(new Action(() => lblMem.Text = memValue.ToString() + " % "));
+            }
+            else
+            {
+                lblMem.Text = memValue.ToString() + " % ";
+            }
+            // -----------------------------------------------------------------------------
+            progressBarCPU.Value = cpuValue;
+            progressBarMEM.Value = memValue;
+        }
+        private static int GetCpuValue()
+        {
+            var CpuCounter = new PerformanceCounter("Processor", " % Processor Time", "_Total");
+            CpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            int returnvalue = (int)CpuCounter.NextValue();
+            return returnvalue;
+        }
+        private static int GetMemValue()
+        {
+            var MemCounter = new PerformanceCounter("Processor", " % Processor Time", "_Total");
+            int returnvalue = (int)MemCounter.NextValue();
+            return returnvalue;
+
+            //var MemCounter = new PerformanceCounter("Memory", "% Commited Bytes in Use");
+            //int returnvalue = (int)MemCounter.NextValue();
+            ///return returnvalue;
+        }
+        // przycisk 
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            if (startflag == false)
+            {
+                startflag = true;
+                _timer.Enabled = true;
+                btn_Start.Text = "STOP";
+            }
+            else
+            {
+                startflag = false;
+                _timer.Enabled = false;
+                btn_Start.Text = "START";
+            }
+        }
+    }
+}
