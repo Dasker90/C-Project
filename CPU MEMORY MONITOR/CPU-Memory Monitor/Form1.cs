78695f1b6690a9d041499c7d2149ef5d9bf89151
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-11-20 12:21:58 +0100

    Update Form1.cs

--- CPU MEMORY MONITOR/CPU-Memory Monitor/Form1.cs
+++ CPU MEMORY MONITOR/CPU-Memory Monitor/Form1.cs
@@ -10,7 +10,7 @@ using System.Timers;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
+// - Monitorowanie Memory CPU
 namespace CPU_Memory_Monitor
 {
     public partial class Form1 : Form
