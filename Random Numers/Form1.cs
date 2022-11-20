3e07e8bcd536b950abe1bafa8b0bfb50383bdf48
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-11-20 12:24:58 +0100

    Update Form1.cs

--- Random Numers/Form1.cs
+++ Random Numers/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// ----
 namespace Quiz_APP
 {
     public partial class Form1 : Form
